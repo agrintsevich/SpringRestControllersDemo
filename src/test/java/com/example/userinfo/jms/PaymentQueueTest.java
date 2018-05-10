97b6a138e56c33b04dd6f81eb4419e473dd5c37d
Anna_Grinzewich <annagrin00&@gmail.com>
2018-05-10 17:14:36 +0300

    Use embedded broker

--- src/test/java/com/example/userinfo/jms/PaymentQueueTest.java
+++ src/test/java/com/example/userinfo/jms/PaymentQueueTest.java
@@ -3,7 +3,10 @@ package com.example.userinfo.jms;
 import com.example.userinfo.model.Payment;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.junit.EmbeddedActiveMQBroker;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +17,17 @@ import org.springframework.jms.support.converter.SimpleMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.example.userinfo.MockDataStore.mockPayment;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PaymentQueueTest {
 
-    @Autowired
+    public static final String DESTINATION_NAME = "payment";
+    public static final String BROKER_URL = "vm://embedded-broker?create=false";
+    @Rule
+    public EmbeddedActiveMQBroker broker = new EmbeddedActiveMQBroker();
+
     protected ActiveMQConnectionFactory activeMQConnectionFactory;
 
     @Autowired
@@ -27,14 +35,28 @@ public class PaymentQueueTest {
 
     @Before
     public void before() {
+        activeMQConnectionFactory = new ActiveMQConnectionFactory(BROKER_URL);
+        activeMQConnectionFactory.setTrustAllPackages(true);
         jmsTemplate.setConnectionFactory(new SingleConnectionFactory(activeMQConnectionFactory));
         jmsTemplate.setMessageConverter(new SimpleMessageConverter());
     }
 
     @Test
     public void testSendPayment() {
-        ActiveMQQueue destination = new ActiveMQQueue("payment");
-        final Payment message = mockPayment();
-        this.jmsTemplate.convertAndSend(destination, message);
+        ActiveMQQueue destination = new ActiveMQQueue(DESTINATION_NAME);
+        final Payment mockPayment = mockPayment();
+        this.jmsTemplate.convertAndSend(destination, mockPayment);
+        jmsTemplate.setReceiveTimeout(5_000);
+        assertReceivedMessage(destination, mockPayment);
+    }
+
+    private void assertReceivedMessage(ActiveMQQueue destination, Payment mockPayment) {
+        Object actualMessage = jmsTemplate.receiveAndConvert(destination);
+        assertThat(actualMessage).isInstanceOf(Payment.class);
+        final Payment actualPayment = (Payment) actualMessage;
+        assertThat(actualPayment.getAmount()).isEqualTo(mockPayment.getAmount());
+        assertThat(actualPayment.getPaymentType()).isEqualTo(mockPayment.getPaymentType());
+        assertThat(actualPayment.getUser()).isNotNull();
+        assertThat(actualPayment.getUser().getId()).isEqualTo(mockPayment.getUser().getId());
     }
 }
